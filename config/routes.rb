43aa10378c9c91feba8649e1c19565f803336a8b
Benjamin <benjaminvanarragon@gmail.com>
2016-11-27 19:53:48 +0000

    Created pages controller, added explore, home, index, and profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+	#define root url
+  root 'pages#index'
+  #define routes for pages
+  get 'pages/home'
+  get 'pages/profile'
+  get 'pages/explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
